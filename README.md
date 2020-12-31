a2c52df323c4ab6217d7854853ad81d314b47f7d
davidolaoluwa360 <d.atebisun@alustudent.com>
2020-12-31 12:00:01 -0800

    new changes to the readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# note-pad
+# Note-Pad
+<p>Note Pad is a simple note taking application that helps users to take note anywhere they are</p>
+
+<h2>Note Pad Attributes<h2>
+<ul>
+    <li>Take Note's</li>
+    <li>Save it to your localstorage</li>
+    <li>Delete if it not needed anymore</li>
+    <li>Add new Note's if you need more</li>
+</ul>
+
+<h2>Neat Interface</h2>
+<img src="./images/note-book-image.PNG" align="center">
+
+<h2>Usage</h2>
+<ul>
+    <li>Git Clone: to clone to your computer</li>
+    <li>Open with your favourite IDE</li>
+</ul>
+
+<h2>Live Demo</h2>
+<a href="https://davidolaoluwa360.github.io/note-pad/" target="_blank">View Note Pad</a>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// author: Atebisun David
+// Project_Github_page: https://davidolaoluwa360.github.io/note-pad/
+
+// Here is a constructor function with some property related to the notepad
 function Note(){  
     "use strict";
     this.key = 1;
@@ -5,12 +9,14 @@ function Note(){
     this.save = false;
 }
 
+// here is a prototype that saves the key and value of user note to the localstorage
 Note.prototype.saveToLS = function(key, value){
     if(this.save){
         localStorage.setItem(key, value);
     }
 };
 
+// here is a prototype of note that remove saved notes from the Localstorage
 Note.prototype.removeFromLS = function(key){
     localStorage.removeItem(key);
 };
@@ -33,12 +39,13 @@ Note.prototype.addNote = function(key = 0, value = "", hasValue = false){
     `;
     noteContainer.appendChild(note);
 
-
+    // here is an event that add new note to the page and assign it a new key
     note.querySelector(".note-add").addEventListener("click", () => {
         this.key++;
         this.addNote();
     });
 
+    // here is an event that save  note to the localstorage
     note.querySelector(".save").addEventListener("click", () =>{
         let key = noteContainer.querySelector(`input[value='${note.querySelector(`input[type="hidden"]`).value}']`).value;
         let value = noteContainer.querySelector(`input[value='${note.querySelector(`input[type="hidden"]`).value}']`).parentNode.querySelector(".note-text-area").innerHTML;
@@ -51,6 +58,7 @@ Note.prototype.addNote = function(key = 0, value = "", hasValue = false){
         }
     });
 
+    // here is an event that close note and also delete it from the localstorage if saved
     note.querySelector(".note-close").addEventListener("click", () => {
         let key = noteContainer.querySelector(`input[value='${note.querySelector(`input[type="hidden"]`).value}']`).value;
         this.removeFromLS(Number.parseInt(key));
@@ -60,6 +68,10 @@ Note.prototype.addNote = function(key = 0, value = "", hasValue = false){
 
 const note = new Note();
 
+// Here before the calling of the note Object
+// 1; a check is been made to the localstorage
+// 2; if the localstorage have some existing value or notes we are displaying it to the user
+// 3; else if there is nothing in the user localstorage we are displaying empty notes to the user
 if(localStorage.length > 0){
     const keys = Object.keys(localStorage);
     keys.forEach(key => {
@@ -69,6 +81,7 @@ if(localStorage.length > 0){
     note.addNote();
 }
 
+// here is an event that add notes to the page
 const startNote = document.querySelector(".add");
 startNote.addEventListener("click", () => {
     note.addNote();
